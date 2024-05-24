e3a84b76cdeafc2b6e4dc27fd4f56297d40e5615
huboh <72440222+huboh@users.noreply.github.com>
2024-05-24 10:05:34 +0100

    refactor: emit empty values from response structure

--- response.go
+++ response.go
@@ -21,7 +21,7 @@ type Response struct {
 	StatusCode int `json:"statusCode"`
 
 	// Data is the response data.
-	Data any `json:"data"`
+	Data any `json:"data,omitempty"`
 
 	// Error is the response error. this field is omitted from the response if it is nil.
 	Error *ResponseError `json:"error,omitempty"`
@@ -30,16 +30,16 @@ type Response struct {
 // Response represent the api error response object.
 type ResponseError struct {
 	// Name is the name of the error.
-	Name string `json:"name"`
+	Name string `json:"name,omitempty"`
 
 	// Cause is the error's cause.
-	Cause string `json:"cause"`
+	Cause string `json:"cause,omitempty"`
 
 	// Stack is the error stack trace.
-	Stack []string `json:"stack"`
+	Stack []string `json:"stack,omitempty"`
 
 	// Message is the error message
-	Message string `json:"message"`
+	Message string `json:"message,omitempty"`
 }
 
 func NewJsonResponseError(msg string, name string, cause string, stack []string) *ResponseError {
