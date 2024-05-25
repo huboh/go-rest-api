ef0ef02831263c70cb86e11c525b466f8a775927
huboh <72440222+huboh@users.noreply.github.com>
2024-05-25 21:07:01 +0100

    refactor: move Route to separate file

--- internal/pkg/router/router.go
+++ internal/pkg/router/router.go
@@ -12,29 +12,6 @@ import (
 	"github.com/huboh/go-rest-api/internal/pkg/utils"
 )
 
-// Route represents an api route
-type Route struct {
-	// Tag is used to add metadata to the route
-	Tag []string
-
-	// Path is request Path
-	Path string
-
-	// Method is the route http request Method
-	Method string
-
-	// Handler is the route http request Handler or a router to handle request received to `path`
-	Handler http.Handler
-}
-
-func NewRoute(m string, p string, h http.Handler) Route {
-	return Route{
-		Path:    p,
-		Method:  m,
-		Handler: h,
-	}
-}
-
 // Router
 type Router struct {
 	// mux is the underlying multiplexer
